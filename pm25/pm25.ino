1749532dd119d714d6b53dc079d2ace3ffd804d9
Anuchit Chalothorn <anoochit@gmail.com>
2019-04-16 21:29:00 +0700

    add oled display

--- pm25/pm25.ino
+++ pm25/pm25.ino
@@ -1,39 +1,83 @@
 #include "PMS.h"
 
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Fonts/FreeSansBold24pt7b.h>
+
 HardwareSerial SerialPMS(1);
 PMS pms(SerialPMS);
 PMS::DATA data;
 
-#define RXD2 23
-#define TXD2 22
+#define RXD2 26
+#define TXD2 25
 
-// ESP32 --> Pantower PMS7003   
+// ESP32 --> Pantower PMS7003
 // 22    --> RX
-// 23    --> TX 
+// 23    --> TX
 // GND   --> GND
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     -1// Reset pin # (or -1 if sharing Arduino reset pin)
+//Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
 
 void setup()
 {
   Serial.begin(9600);
   SerialPMS.begin(9600, SERIAL_8N1, RXD2, TXD2);
+  pms.passiveMode();
+
+  display.setFont(&FreeSansBold24pt7b);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
 
 }
 
 void loop()
 {
-  if (pms.read(data))
+  //Serial.println("Waking up, wait 30 seconds for stable readings...");
+  pms.wakeUp();
+  delay(3000);
+
+  //Serial.println("Send read request...");
+  pms.requestRead();
+
+  //Serial.println("Wait max. 1 second for read...");
+  if (pms.readUntil(data))
   {
-    Serial.print("PM 1.0 (ug/m3): ");
-    Serial.println(data.PM_AE_UG_1_0);
+//
+//    Serial.print("PM 1.0 (ug/m3): ");
+//    Serial.println(data.PM_AE_UG_1_0);
+//
+//    Serial.print("PM 2.5 (ug/m3): ");
+//    Serial.println(data.PM_AE_UG_2_5);
+//
+//
+//    Serial.print("PM 10.0 (ug/m3): ");
+//    Serial.println(data.PM_AE_UG_10_0);
 
-    Serial.print("PM 2.5 (ug/m3): ");
-    Serial.println(data.PM_AE_UG_2_5);
+    display.clearDisplay();
+    display.setTextColor(WHITE, BLACK);  //Text is white ,background is black
+    display.setTextSize(1);
+    display.setCursor(35, 50);
+    display.print(data.PM_AE_UG_2_5);
+    display.display();
+    delay(10000);
 
-    Serial.print("PM 10.0 (ug/m3): ");
-    Serial.println(data.PM_AE_UG_10_0);
-
-    Serial.println();
   }
+  else
+  {
+    Serial.println("No data.");
+  }
+
+  //  Serial.println("Going to sleep for 60 seconds.");
+  pms.sleep();
+  delay(60000);
 
   // Do other stuff...
 }
